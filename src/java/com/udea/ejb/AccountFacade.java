3e9b49eb53a2fe0a4f2590b76835bccf0bdb228c
luis.velasquezg <luis.velasquezg@udea.edu.co>
2018-08-07 22:31:56 -0500

    Creacion del Entity Bean y del EJB

---
+++ src/java/com/udea/ejb/AccountFacade.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.udea.ejb;
+
+import com.udea.entity.Account;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author Luis
+ */
+@Stateless
+public class AccountFacade extends AbstractFacade<Account> implements AccountFacadeLocal {
+
+    @PersistenceContext(unitName = "CuentasWebPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public AccountFacade() {
+        super(Account.class);
+    }
+
+    @Override
+    public boolean checkLogin(String u, String p) {
+        Query q = em.createQuery("select a from Account a "
+            + "where a.username=:u and a.password=:p");
+        q.setParameter("u", u);
+        q.setParameter("p", p);
+        return q.getResultList().size()>0;
+    }
+    
+}
