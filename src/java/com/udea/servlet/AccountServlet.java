99c3d6f5d30646410901a912060d7f1be9ca8e99
luis.velasquezg <luis.velasquezg@udea.edu.co>
2018-08-08 13:15:36 -0500

    Creacion del Servlet

---
+++ src/java/com/udea/servlet/AccountServlet.java
@@ -0,0 +1,174 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.udea.servlet;
+
+import com.udea.ejb.AccountFacadeLocal;
+import com.udea.entity.Account;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Luis
+ */
+public class AccountServlet extends HttpServlet {
+
+    @EJB
+    private AccountFacadeLocal accountFacade;
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            String action = request.getParameter("action");
+            String url = "index.jsp";
+            if (null != action) switch (action) {
+                case "list":
+                    List<Account> findAll = accountFacade.findAll();
+                    request.getSession().setAttribute("accounts", findAll);
+                    url = "listAccounts.jsp";
+                    break;
+                case "login":
+                    String u = request.getParameter("username");
+                    String p = request.getParameter("password");
+                    boolean checklogin = accountFacade.checkLogin(u, p);
+                    if (checklogin) {
+                        request.getSession().setAttribute("login", u);
+                        url = "manager.jsp";
+                    } else {
+                        url = "login.jsp?error=1";
+                    }   break;
+                case "insert":{
+                    Account a = new Account();
+                    a.setUsername(request.getParameter("username"));
+                    a.setPassword(request.getParameter("password"));
+                    a.setEmail(request.getParameter("email"));
+                    accountFacade.create(a);
+                    url = "login.jsp";
+                        break;
+                    }
+                case "delete":{
+                    String id = request.getParameter("id");
+                    Account a = accountFacade.find(Integer.valueOf(id));
+                    accountFacade.remove(a);
+                    url = "AccountServlet?action=list";
+                        break;
+                    }
+                case "logout":
+                    request.getSession().removeAttribute("login");
+                    url = "login.jsp";
+                    break;
+                default:
+                    break;
+            }
+            
+            //<editor-fold defaultstate="collapsed" desc="Antigua estructura IF">
+            //            if ("list".equals(action)) {
+            //                List<Account> findAll = accountFacade.findAll();
+            //                request.getSession().setAttribute("accounts", findAll);
+            //                url = "listAccounts.jsp";
+            //            } else if ("login".equals(action)) {
+            //                String u = request.getParameter("username");
+            //                String p = request.getParameter("password");
+            //                boolean checklogin = accountFacade.checkLogin(u, p);
+            //                if (checklogin) {
+            //                    request.getSession().setAttribute("login", u);
+            //                    url = "manager.jsp";
+            //                } else {
+            //                    url = "login.jsp?error=1";
+            //                }
+            //            } else if ("insert".equals(action)) {
+            //                Account a = new Account();
+            //                a.setUsername(request.getParameter("username"));
+            //                a.setPassword(request.getParameter("password"));
+            //                a.setEmail(request.getParameter("email"));
+            //                accountFacade.create(a);
+            //                url = "login.jsp";
+            //            } else if ("delete".equals(action)) {
+            //                String id = request.getParameter("id");
+            //                Account a = accountFacade.find(Integer.valueOf(id));
+            //                accountFacade.remove(a);
+            //                url = "AccountServlet?action=list";
+            //            } else if ("logout".equals(action)) {
+            //                request.getSession().removeAttribute("login");
+            //                url = "login.jsp";
+            //            }
+            //</editor-fold>
+            
+            response.sendRedirect(url);
+        } finally {
+            out.close();
+        }
+//        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+//            out.println("<!DOCTYPE html>");
+//            out.println("<html>");
+//            out.println("<head>");
+//            out.println("<title>Servlet AccountServlet</title>");            
+//            out.println("</head>");
+//            out.println("<body>");
+//            out.println("<h1>Servlet AccountServlet at " + request.getContextPath() + "</h1>");
+//            out.println("</body>");
+//            out.println("</html>");
+//        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
